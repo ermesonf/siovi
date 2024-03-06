04cdad323a06de3a33c5b85600c3da703360d07e
ermesonf <ermeson.freitas@hotmail.com>
2024-03-06 00:58:16 -0300

    feat: add keys

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(worksheet="test")
+
+# Convertendo os dados para um DataFrame do pandas
+df = pd.DataFrame(data)
+
+# Removendo as linhas e colunas vazias
+df = df.dropna(how='all')
+
+st.dataframe(df)
