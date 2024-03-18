6f6e291269d901a638fed98267cd1a37ccd7f488
ermesonf <ermeson.freitas@hotmail.com>
2024-03-17 23:12:05 -0300

    teste

--- src/form_app.py
+++ src/form_app.py
@@ -9,7 +9,7 @@ import yaml
 from yaml.loader import SafeLoader
 
 # --- USER AUTHENTICATION ---
-with open('./config.yaml') as file:
+with open("./config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
