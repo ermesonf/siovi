46cef61e3af98575373210a082ec1688dae1f804
ermesonf <ermeson.freitas@hotmail.com>
2024-03-25 10:10:29 -0300

    remove file

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(worksheet="test")
 
-# Convertendo os dados para um DataFrame do pandas
+# Convertendo os dados para um DataFrame do Pandas
 df = pd.DataFrame(data)
 
 # Removendo as linhas e colunas vazias
